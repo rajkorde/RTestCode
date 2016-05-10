e563f8a75d3539d3e8b6ddd2e97711b422349786
Rajesh Korde <Rajesh Korde>
2016-05-09 22:28:46 -0700

    minor edits

--- Scripts/Test.R
+++ Scripts/Test.R
@@ -1,1 +1,19 @@
-plot(rnorm(100))
+library(pwr)
+
+#T test sample size
+#pwr.t.test(n=11165, sig.level = 0.95, power = 0.8)
+
+#prop test sample size
+pwr.2p.test(h=ES.h(0.04,0.05), sig.level = 0.05, power = 0.8)
+
+power.prop.test(p1=0.04, p2=0.05, sig.level=0.05, power = 0.95, alternative="two.sided")
+
+#running prop test
+prop.test(20, 50, 0.3)
+prop.test(200, 500, 0.3)
+
+smokers  <- c( 83, 90, 129, 70 )
+patients <- c( 86, 93, 136, 82 )
+prop.test(smokers, patients)
+
+
