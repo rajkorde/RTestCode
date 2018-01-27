6149b02d8dfea355b250d148b21549f646cac675
rajkorde <rkorde39@hotmail.com>
2018-01-27 14:54:05 -0800

    minor edits

--- Scripts/Test.R
+++ Scripts/Test.R
@@ -1,10 +1,11 @@
-c1 <- c()
+library(jsonlite)
 
-f <- function(x) {
-  c1 <<- c(c1, x) 
+d <- readLines("D:/Temp/AFSDump-11142017/11142017/AFSDump_AAD_156044517231.27.json")
+
+library(qdapTools)
+x <- fromJSON(d[1])
+y <- stack(x)
+
+for line in d {
+#  x <- bind_rows(x, fromJSON(line))
 }
-
-f(5)
-f(10)
-c1
-f(11)
--- Scripts/survey.R
+++ Scripts/survey.R
@@ -50,11 +50,8 @@ svyquantile(~ue91, province.design, quantiles = 0.5, ci = TRUE)
 
 #ex1 post-stratification
 #http://www.ats.ucla.edu/stat/r/faq/svy_r_post.htm
-mydata <- 
-  read.dta( 
-    "http://www.ats.ucla.edu/stat/data/dogcats.dta" , 
-    convert.factors = FALSE 
-  )
+mydata <- read.dta("http://www.ats.ucla.edu/stat/data/dogcats.dta")
+mydata <- read.dta13("http://www.ats.ucla.edu/stat/data/dogcats.dta")
 mydata$fpc <- 1300
 
 
@@ -143,3 +140,103 @@ summary(weights(small.svy.rake))
 #use trimWeights() if the weights are <0.3 or >3
 #small.svy.rake.trim <- trimWeights(small.svy.rake, lower=0.3, upper=3, strict=TRUE) 
 
+### http://r-survey.r-forge.r-project.org/survey/example-poststrat.html
+data(api)
+dclus1<-svydesign(id=~dnum, weights=~pw, data=apiclus1, fpc=~fpc)
+rclus1<-as.svrepdesign(dclus1)
+
+svymean(~api00, rclus1)
+svytotal(~enroll, rclus1)
+
+pop.types <- data.frame(stype=c("E","H","M"), Freq=c(4421,755,1018))
+rclus1p<-postStratify(rclus1, ~stype, pop.types)
+summary(rclus1p)
+
+svymean(~api00, rclus1p)
+svytotal(~enroll, rclus1p)
+
+## and for svydesign objects
+dclus1p<-postStratify(dclus1, ~stype, pop.types)
+summary(dclus1p)
+svymean(~api00, dclus1p)
+svytotal(~enroll, dclus1p)
+
+### Raking
+data(api)
+dclus1 <- svydesign(id=~dnum, weights=~pw, data=apiclus1, fpc=~fpc)
+rclus1 <- as.svrepdesign(dclus1)
+svymean(~api00, rclus1)
+svytotal(~enroll, rclus1)
+
+pop.types <- data.frame(stype=c("E","H","M"), Freq=c(4421,755,1018))
+pop.schwide <- data.frame(sch.wide=c("No","Yes"), Freq=c(1072,5122))
+
+rclus1r <- rake(rclus1, list(~stype,~sch.wide), list(pop.types, pop.schwide))
+
+svymean(~api00, rclus1r)
+svytotal(~enroll, rclus1r)
+
+xtabs(~stype, apipop)
+svytable(~stype, rclus1r, round=TRUE)
+xtabs(~sch.wide, apipop)
+svytable(~sch.wide, rclus1r, round=TRUE)
+
+xtabs(~stype+sch.wide, apipop)
+svytable(~stype+sch.wide, rclus1r, round=TRUE)
+
+## Do it for a design without replicate weights
+dclus1r<-rake(dclus1, list(~stype,~sch.wide), list(pop.types, pop.schwide))
+svymean(~api00, dclus1r)
+svytotal(~enroll, dclus1r)
+
+## compare to raking with calibrate()
+dclus1gr<-calibrate(dclus1, ~stype+sch.wide, pop=c(6194, 755,1018,5122),
+                    calfun="raking")
+svymean(~stype+api00, dclus1r)
+svymean(~stype+api00, dclus1gr)
+
+## compare to joint post-stratification
+## (only possible if joint population table is known)
+##
+pop.table <- xtabs(~stype+sch.wide,apipop)
+rclus1ps <- postStratify(rclus1, ~stype+sch.wide, pop.table)
+svytable(~stype+sch.wide, rclus1ps, round=TRUE)
+svymean(~api00, rclus1ps)
+svytotal(~enroll, rclus1ps)
+
+## Example of raking with partial joint distributions
+pop.imp<-data.frame(comp.imp=c("No","Yes"),Freq=c(1712,4482))
+dclus1r2<-rake(dclus1, list(~stype+sch.wide, ~comp.imp),
+               list(pop.table, pop.imp))
+svymean(~api00, dclus1r2)
+
+## compare to calibrate() syntax with tables
+dclus1r2<-calibrate(dclus1, formula=list(~stype+sch.wide, ~comp.imp),
+                    population=list(pop.table, pop.imp),calfun="raking")
+svymean(~api00, dclus1r2)
+
+###calibration
+
+data(api)
+dclus1<-svydesign(id=~dnum, weights=~pw, data=apiclus1, fpc=~fpc)
+svymean(~api00, dclus1)
+pop.totals<-c(`(Intercept)`=6194, stypeH=755, stypeM=1018)
+
+(dclus1g<-calibrate(dclus1, ~stype, pop.totals))
+svymean(~api00, dclus1g)
+svytotal(~enroll, dclus1g)
+svytotal(~stype, dclus1g)
+
+## Make weights constant within school district
+(dclus1agg<-calibrate(dclus1, ~stype, pop.totals, aggregate=1))
+svymean(~api00, dclus1agg)
+svytotal(~enroll, dclus1agg)
+svytotal(~stype, dclus1agg)
+
+## Now add sch.wide
+(dclus1g2 <- calibrate(dclus1, ~stype+sch.wide, c(pop.totals, sch.wideYes=5122)))
+
+svymean(~api00, dclus1g2)
+svytotal(~enroll, dclus1g2)
+svytotal(~stype, dclus1g2)
+svymean(~api00, dclus1g)
