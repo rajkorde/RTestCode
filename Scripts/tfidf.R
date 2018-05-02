6918a46c1c1753a5b6c3722f36e492a119310e75
NTDEV\rajkorde <rajkorde@ntdev.microsoft.com>
2018-05-02 16:50:55 -0700

    minor edits

--- Scripts/tfidf.R
+++ Scripts/tfidf.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(janeaustenr)
 library(tidytext)
+library(ggplot2)
 
 book_words <- austen_books() %>%
   unnest_tokens(word, text) %>%
@@ -15,3 +16,20 @@ book_words <- left_join(book_words, total_words)
 
 book_words <- book_words %>%
   bind_tf_idf(word, book, n)
+
+
+book_words %>%
+  select(-total) %>%
+  arrange(desc(tf_idf))
+
+book_words %>%
+  arrange(desc(tf_idf)) %>%
+  mutate(word = factor(word, levels = rev(unique(word)))) %>% 
+  group_by(book) %>% 
+  top_n(15) %>% 
+  ungroup %>%
+  ggplot(aes(word, tf_idf, fill = book)) +
+  geom_col(show.legend = FALSE) +
+  labs(x = NULL, y = "tf-idf") +
+  facet_wrap(~book, ncol = 2, scales = "free") +
+  coord_flip()
