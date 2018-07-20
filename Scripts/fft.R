76f344f6b7414f04ad6a0bcb5b59faa3908deb36
NTDEV\rajkorde <rajkorde@ntdev.microsoft.com>
2018-07-20 14:17:26 -0700

    added complete_ts() and simulate_ts()

--- Scripts/fft.R
+++ Scripts/fft.R
@@ -3,6 +3,10 @@
 library(TSA)
 library(lubridate)
 library(tidyverse)
+library(zoo)
+
+options(scipen = 9999)
+
 raw <- read.csv("C:/Users/rajkorde.NTDEV/Downloads/20131120-20151110-google-analytics.csv")
 p = periodogram(raw$Visite)
 
@@ -21,9 +25,47 @@ simulate_timestamps <- function(starttime = ymd_hms("2018-07-01 8:05:23 AM"),
   starttime + minutes(as.integer((1:n)*periodicity*24*60 + (2*(runif(n) - 0.5))*jitter))
 }
 
-x <- simulate_timestamps(periodicity = 15)
+complete_ts <- function(ts, start = ymd_hms("2018-07-05 1:00:00 AM"),
+                        end = ymd_hms("2018-08-05 23:00:00 PM")) {
+  allts <- seq.POSIXt(as.POSIXct(start, tz="UTC"), as.POSIXct(end, tz="UTC"), by = "hours")
+  
+  for (i in 1:length(ts)) {
+    minute(ts[i]) <- 0
+    second(ts[i]) <- 0
+  }
+  
+  data.frame(TimeStamp = allts) %>%
+    mutate(Event = ifelse(TimeStamp %in% x1, 1, 0))
+}
 
+x <- simulate_timestamps(n = 5)
+dx <- complete_ts(x)
+plot(zoo(dx$Event, dx$TimeStamp))
 
-p <- periodogram(ts(x))
+a <- acf(dx$Event, lag.max = 200)
+
+tibble(lag = a$lag[, 1, 1], acf = a$acf[, 1, 1]) %>%
+  arrange(desc(acf)) %>%
+  top_n(1)
+
+data.frame(Date = x) %>%
+  arrange(Date) %>%
+  mutate(Next = lead(Date)) %>%
+  mutate(Diff = as.numeric(difftime(Next, Date, units = c("mins"))))
+
+z <- zoo(dx$Event, dx$TimeStamp)
+plot(z)
+
+p <- periodogram(z)
 data.frame(freq=p$freq, spec=p$spec) %>%
   arrange(desc(spec))
+
+acf(zoo(x))
+
+dx <- data.frame(Date = as.Date(x), Event = 1)
+
+dx <- data.frame(Date = seq.Date(as.Date(min(x)), as.Date(max(x)), by="day")) %>%
+             left_join(dx) %>%
+  replace_na(list(Event = 0))
+  
+
