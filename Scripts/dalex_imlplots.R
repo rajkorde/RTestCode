96ce75779133c43c864be05ae4dc58f345a4602b
rajkorde <rkorde39@hotmail.com>
2018-09-23 09:38:24 -0700

    added dalex and imlplots for ceterus paribus and wangkardu plots

--- Scripts/dalex_imlplots.R
+++ Scripts/dalex_imlplots.R
@@ -1,6 +1,8 @@
 library(DALEX)
 library(caret)
+
 data("apartments")
+data("apartmentsTest")
 
 set.seed(123)
 
@@ -12,7 +14,7 @@ regr_nn <- train(m2.price~., data = apartments, method = "nnet",
                  tuneGrid = expand.grid(size = 2, decay = 0),
                  trControl = trainControl(method = "none", seeds = 1))
 
-data("apartmentsTest")
+
 explainer_regr_rf <- explain(regr_rf, label = "rf", 
                              data = apartmentsTest,
                              y = apartmentsTest$m2.price)
@@ -26,7 +28,6 @@ explainer_regr_nn <- explain(regr_nn, label = "nn",
                              y = apartmentsTest$m2.price)
 
 # Model performance
-
 mp_regr_rf <- model_performance(explainer_regr_rf)
 mp_regr_gbm <- model_performance(explainer_regr_gbm)
 mp_regr_nn <- model_performance(explainer_regr_nn)
@@ -59,11 +60,57 @@ mpp_regr_nn  <- variable_response(explainer_regr_nn, variable =  "district", typ
 
 plot(mpp_regr_rf, mpp_regr_gbm, mpp_regr_nn)
 
-library(imlplots)
-print(summarizeColumns(b)[, -c(5, 6, 7)], digits = 4)
+# ceteris paribus plot
+# ceteris paribus - with other conditions remaining the same.
+# explore local behavior of a model
 
-boston.task = makeRegrTask(data = b, target = "medv")
-rf.mod = train("regr.randomForest", boston.task)
-glm.mod = train("regr.glm", boston.task)
-mod.list = list(rf.mod, glm.mod)
-imlplots(data = boston, task = boston.task, models = mod.list)
+library("ceterisParibus")
+
+explainer_rf <- explain(regr_rf,
+                        data = apartmentsTest, 
+                        y = apartmentsTest$m2.price)
+apartments_small_1 <- apartmentsTest[1,]
+apartments_small_2 <- select_sample(apartmentsTest, n = 20)
+
+cp_rf_y1 <- ceteris_paribus(explainer_rf, apartments_small_1, 
+                            y = apartments_small_1$m2.price)
+cp_rf_y2 <- ceteris_paribus(explainer_rf, apartments_small_2, 
+                            y = apartments_small_2$m2.price)
+
+plot(cp_rf_y1, show_profiles = TRUE, show_observations = TRUE, show_rugs = TRUE,
+     show_residuals = TRUE, alpha = 0.5, size_points = 3,
+     alpha_points = 1, size_rugs = 0.5)
+plot(cp_rf_y2, show_profiles = TRUE, show_observations = TRUE, show_rugs = TRUE,
+     alpha = 0.2, alpha_points = 1, size_rugs = 0.5)
+#plot(cp_rf_y1, show_profiles = TRUE, show_observations = TRUE, show_rugs = TRUE,
+#     show_residuals = TRUE, alpha = 0.5, 
+#     color = "construction.year", size_points = 3)
+#plot(cp_rf_y2, show_profiles = TRUE, show_observations = TRUE, show_rugs = TRUE,
+#     size = 0.5, alpha = 0.5, color = "surface")
+
+# wangkardu plots
+# explore local goodness of fit
+
+cr_rf <- local_fit(explainer_rf, observation = apartments_small_1,
+                   select_points = 0.002, selected_variable = "surface")
+plot(cr_rf)
+
+# breakdown plots - distribution changes
+br_rf <- prediction_breakdown(explainer_rf, observation = apartments_small_1)
+plot(br_rf)
+
+
+#modelDown
+#devtools::install_github("MI2DataLab/modelDown")
+library(modelDown)
+modelDown::modelDown(explainer_regr_rf, explainer_regr_gbm)
+
+
+#devtools::install_github('compstat-lmu/imlplots')
+library(imlplots)
+
+apartments_task <- makeRegrTask(data = apartments, target = "m2.price")
+rf_model <- train("regr.randomForest", apartments_task)
+glm_model <- train("regr.glm", apartments_task)
+model_list <- list(rf_model, glm_model)
+imlplots(data = apartments, task = apartments_task, models = model_list)
